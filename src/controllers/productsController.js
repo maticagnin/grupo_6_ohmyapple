667e448f9be6b95e582e000e15ac76a60f79aebb
maticagnin <matiascagnin95@gmail.com>
2022-11-12 12:46:29 -0300

    Creacion y Edición de productos ejs y rutas

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -3,6 +3,12 @@ const path = require('path');
 const productsController = {
     detalle: (req, res) => {
         res.render('detalledeproducto')
+    },
+    creacion: (req, res) => {
+        res.render('creacionDeProducto')
+    },
+    edicion: (req, res) => {
+        res.render('edicionDeProducto')
     }
 
 };
--- src/routes/products.js
+++ src/routes/products.js
@@ -5,6 +5,7 @@ const productsController = require('../controllers/productsController.js');
 let router = express.Router();
 
 router.get('/detalledeproducto', productsController.detalle);
-
+router.get('/creacionDeProducto', productsController.creacion);
+router.get('/edicionDeProducto', productsController.edicion);
 
 module.exports = router;
