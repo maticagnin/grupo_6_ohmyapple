b71153cf463e0aacea6b7a875a79a4c4b51e5750
rociocabezai <rocio.cabezai@gmail.com>
2022-11-26 20:42:48 -0300

    Cambios en rutas.

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,12 +4,10 @@ const productsController = require('../controllers/productsController.js');
 
 let router = express.Router();
 
-router.get('/:idProducto', productsController.detalle);
+router.get('/', productsController.productos);
 router.get('/creacion', productsController.creacion);
 router.get('/edicion', productsController.edicion);
 router.get('/carrito', productsController.carrito);
-router.get('/', productsController.productos);
-
-
+router.get('/:idProducto', productsController.detalle);
 
 module.exports = router;
