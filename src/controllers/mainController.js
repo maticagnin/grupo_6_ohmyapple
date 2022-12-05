e70a776c064454ddba580aa3bbfc4792f9f1ac5d
rociocabezai <rocio.cabezai@gmail.com>
2022-12-05 11:10:38 -0300

    Probando barra de busqueda.

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,36 @@
+const fs = require('fs');
 const path = require('path');
+
+const productsFilePath = path.resolve(__dirname + '/../data/productos.json');
+const productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 
 const mainController = {
     index: (req, res) => {
         res.render('home')
     },
+    buscar: (req, res) => {
+        let consulta = req.query.consulta
+
+        let productosFiltrados = []
+
+        for (let i = 0; i < productos.length; i++){
+            if(productos[i].nombre.includes(consulta)){
+                productosFiltrados.push(productos[i]);   
+            }
+        }
+        let catIphone = productosFiltrados.filter( prodIphone => prodIphone.categoria == 'iPhone')
+        let catMac = productosFiltrados.filter( prodMac => prodMac.categoria == 'Mac')
+        let catAirpods = productosFiltrados.filter( prodAirpods => prodAirpods.categoria == 'AirPods')
+        let catIpad = productosFiltrados.filter( prodIpad => prodIpad.categoria == 'iPad')
+        let catWatch = productosFiltrados.filter( prodWatch => prodWatch.categoria == 'Watch')
+        let catAccesorios = productosFiltrados.filter( prodAccesorios => prodAccesorios.categoria == 'Accesorios')
+    
+        res.render('productos', {catIphone, catMac, catAirpods, catIpad, catWatch, catAccesorios, toThousand})
+    
+    }
 
 };
 
--- src/routes/main.js
+++ src/routes/main.js
@@ -5,5 +5,6 @@ const mainController = require('../controllers/mainController.js');
 let router = express.Router();
 
 router.get('/', mainController.index);
+router.get('/buscar', mainController.buscar);
 
 module.exports = router;
