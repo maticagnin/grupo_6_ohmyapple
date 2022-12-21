7d49b697fa226f816cc3d64c75105b8aeb120a6b
rociocabezai <rocio.cabezai@gmail.com>
2022-12-20 23:24:32 -0300

    Boton de recordar en Login. Cookie Parser en App.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(methodOverride('_method'));
+app.use(cookieParser());
 
 app.set('views', path.join('src/views'));
 
