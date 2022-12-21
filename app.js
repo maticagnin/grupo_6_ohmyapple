cc10c3e1c1acef793adb94795c132d427424de9d
rociocabezai <rocio.cabezai@gmail.com>
2022-12-20 23:29:50 -0300

    Cookie Parser instalado.

--- app.js
+++ app.js
@@ -5,20 +5,21 @@ const path = require ('path');
 const app = express();
 
 const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(cookieParser());
-
 app.set('views', path.join('src/views'));
 
 app.set('view engine', 'ejs');
 
 const mainRutas = require("./src/routes/main.js")
 const productsRutas = require("./src/routes/products.js")
-const userRutas = require("./src/routes/users.js")
+const userRutas = require("./src/routes/users.js");
 
 app.use("/", mainRutas);
 app.use("/productos", productsRutas);
