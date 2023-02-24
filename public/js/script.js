03cec191acb82c07cbca4c7c68bbf6206f5893e7
maticagnin <matiascagnin95@gmail.com>
2023-02-23 21:24:33 -0300

    Validaciones Front

--- public/js/script.js
+++ public/js/script.js
@@ -15,6 +15,78 @@ window.addEventListener("load", function(){
             }
         })
 
+    let formulario = document.querySelector("form.register-form-box")
 
+    formulario.addEventListener("submit", function (e) {
+    
+        let errores = [];
+
+        let campoNombre_apellido = document.querySelector("input.nombre_apellido");
+
+        if(campoNombre_apellido.value == ""){
+            errores.push("El campo Nombre y Apellido debe estar completo");
+        }else if (campoNombre_apellido.value.length < 2){
+            errores.push("El campo Nombre y Apellido debe tener mínimo 2 caracteres")
+        }
+
+        let campoEmail = document.querySelector("input.email");
+
+        if(campoEmail.value == ""){
+            errores.push("El campo Email debe estar completo");
+        }
+
+        let campoPass = document.querySelector("input.inputpass");
+
+        if(campoPass.value == ""){
+            errores.push("El campo Contraseña debe estar completo");
+        }else if (campoPass.value.length < 8){
+            errores.push("El campo Contraseña debe tener mínimo 8 caracteres")
+        }
+
+        if (errores.length > 0){
+            e.preventDefault();
+
+            let ulErrores = document.querySelector("div.errores ul");
+            for(let i = 0; i < errores.length; i++){
+                ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
+            }
+        }
+ 
+    })
+})
+
+    window.addEventListener("load", function(){
+        
+    let formularioCreacion = document.querySelector("form.creacion-formulario")
+
+    formularioCreacion.addEventListener("submit", function (e) {
+    
+        let errores = [];
+
+        let campoNombre = document.querySelector(".nombre");
+
+        if(campoNombre.value == ""){
+            errores.push("El campo Nombre debe estar completo");
+        }else if (campoNombre.value.length < 5){
+            errores.push("El campo Nombre debe tener mínimo 5 caracteres")
+        }
+
+        let campoDescripcion = document.querySelector(".descripcion");
+
+        if(campoDescripcion.value.length < 20){
+            errores.push("El campo Descripción debe tener mínimo 20 caracteres")
+        }
+
+        if (errores.length > 0){
+            e.preventDefault();
+
+            let ulErrores = document.querySelector("div.errores ul");
+            for(let i = 0; i < errores.length; i++){
+                ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
+            }
+        }
+ 
+    })
+ 
 
 })
